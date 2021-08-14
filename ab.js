e6cfea75e32ae915a2c2f843b3003544f38cb091
Neelakandan <86792263+Neelakandan839@users.noreply.github.com>
2021-08-14 14:18:20 +0530

    Delete ab.js

--- ab.js
+++
@@ -1,8 +0,0 @@
-var req=new XMLHttpRequest();
-var url='https://api.covid19api.com/summary';
-req.open('GET',url,true)
-req.send();
-req.onload=function(){
-var data=JSON.parse(this.response);
-console.log(data.Global);
-}
